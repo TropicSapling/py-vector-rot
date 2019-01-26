64163f02fa2cfb6f0e255e5bca8a1cc04d29e205
TropicSapling <tropical.productions.scratch@gmail.com>
2019-01-26 19:26:31 +0100

    Now you can see the vector rotating

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
+from time import sleep
+
 vector = np.array([5, 0])
 
 v = math.radians(float(input("Rotation in degrees: ")))
@@ -11,14 +13,17 @@ the_matrix = np.array([[math.cos(v), -math.sin(v)], [math.sin(v), math.cos(v)]])
 
 rotated_vector = vector
 for _ in range(count):
+	plt.clf()
+	plt.xlim(-10, 10)
+	plt.ylim(-10, 10)
+	
 	rotated_vector = the_matrix.dot(rotated_vector)
-
-plt.quiver([0, 0], [0, 0], [vector[0], rotated_vector[0]], [vector[1], rotated_vector[1]], color=['r', 'g'], angles='xy', scale_units='xy', scale=1)
-
-plt.xlim(-10, 10)
-plt.ylim(-10, 10)
-
-plt.annotate('original vector', xy=(vector[0] + 0.5, vector[1] - 0.25))
-plt.annotate('rotated vector', xy=(rotated_vector[0] + 0.5, rotated_vector[1] - 0.25))
-
+	
+	plt.quiver([0, 0], [0, 0], [vector[0], rotated_vector[0]], [vector[1], rotated_vector[1]], color=['r', 'g'], angles='xy', scale_units='xy', scale=1)
+	
+	plt.annotate('original vector', xy=(vector[0] + 0.5, vector[1] - 0.25))
+	plt.annotate('rotated vector', xy=(rotated_vector[0] + 0.5, rotated_vector[1] - 0.25))
+	
+	plt.pause(0.25)
+	
 plt.show()
